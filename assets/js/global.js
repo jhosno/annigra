6c26c50d9460d257d2533b1b693aaf6197a90928
jhosno <jhosnoirlit@gmail.com>
2022-10-28 17:24:13 -0400

    update:menu

--- assets/js/global.js
+++ assets/js/global.js
@@ -31,3 +31,24 @@ function topFunction() {
 let contact_name = this.document.getElementById('__contact_form_name')
 let contact_tel = this.document.getElementById('__contact_form_telephone')
 let contact_email = this.document.getElementById('__contact_form_email')
+
+//menu function 
+let open_side_menu = this.document.getElementById('__anni_menu_opener');
+let close_side_menu = this.document.getElementById('__anni_close_side_menu');
+let side_menu = this.document.getElementById('__anni_side_menu_area');
+let menu_icon_has_hover = this.document.getElementsByClassName('__anni_icon_has_hover');
+close_side_menu.addEventListener('click', ()=>{
+  side_menu.style.visibility = "hidden";
+  console.log('hode')
+
+});
+open_side_menu.addEventListener('click', ()=>{
+  side_menu.style.visibility = "visible";
+  console.log('visible')
+});
+
+open_side_menu.addEventListener("mouseover", e => {
+    console.log("mouse over test!");
+    open_side_menu
+  }
+    , false);
