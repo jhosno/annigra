3cfe46dfd57a20ccc8b7984bab64a857e4da2763
jhosno <jhosnoirlit@gmail.com>
2022-10-28 13:32:43 -0400

    update: form, faq, bg number effect, back to top btn

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,33 @@
-window.addEventListener('load', ()=>{
+
+//loader
+window.addEventListener('load', () => {
     this.document.getElementById('loader').classList.toggle('loader-out')
 })
+
+//back to up btn
+let backToUpBtn = document.getElementById('__btn-back-to-top');
+
+window.onscroll = () => {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToUpBtn.style.display = "block";
+    } else {
+        backToUpBtn.style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+//form validation
+
+let contact_name = this.document.getElementById('__contact_form_name')
+let contact_tel = this.document.getElementById('__contact_form_telephone')
+let contact_email = this.document.getElementById('__contact_form_email')
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,20 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
     let currentHour = new Date().getHours()
+    let greeting = this.document.getElementById('greeting')
+    let greeting_bg = this.document.getElementById('__home_section')
     console.log(currentHour);
-    if(currentHour <= 12){
+    if (currentHour <= 12) {
         console.log('morning')
-        this.document.getElementById('greeting').innerHTML='good morning'
+        greeting.innerHTML = 'good morning'
+        greeting_bg.style.background = "url('/assets/img/day.jpg')"
         
     }
-    if(currentHour <= 18 && currentHour > 12){
+    if (currentHour <= 18 && currentHour > 12) {
         console.log('afternoon')
-        this.document.getElementById('greeting').innerHTML='good afternoon'
-
+        greeting.innerHTML = 'good afternoon'
+        greeting_bg.style.background = "url('/assets/img/Evening.png')"
+        
     }
-    if(currentHour > 18 && currentHour <= 23){
+    if (currentHour > 18 && currentHour <= 23) {
         console.log('evening')
-        this.document.getElementById('greeting').innerHTML='good evening'
+        greeting.innerHTML = 'good evening'
+        greeting_bg.style.background = "url('/assets/img/night.jpg')"
 
     }
-  
-  });
+
+});
